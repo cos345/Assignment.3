db5c40eb10c0c416e9d5184e331ebd904e87d294
cos345 <craig.osullivan@ymail.com>
2021-11-18 11:08:26 +0000

    Initial commit

---
+++ src/main/java/com/example/assignment3/HelloApplication.java
@@ -0,0 +1,80 @@
+package com.example.assignment3;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        primaryStage.show();
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+        Label email = new Label("Email:");
+        grid.add(email, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (email.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, grid.getScene().getWindow(),
+                            "Form Error!", "Please enter your email id");
+                    return;
+                }
+            }
+        });
+    }
+
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+        public static void main (String[]args){
+            launch(args);
+        }
+    }
