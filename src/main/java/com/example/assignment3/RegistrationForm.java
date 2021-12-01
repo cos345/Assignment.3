3bb8a6ab6fe6aa417c61d2b96e9dc2aec7dca855
cos345 <craig.osullivan@ymail.com>
2021-12-01 09:49:52 +0000

    Added checkEmail method, TestValidEmail now passes

--- src/main/java/com/example/assignment3/RegistrationForm.java
+++ src/main/java/com/example/assignment3/RegistrationForm.java
@@ -16,9 +16,17 @@ import javafx.stage.Window;
 
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationForm extends Application {
     private Stage stage;
+
+    public static boolean checkEmail(String email){
+        final Pattern VALID_EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_REGEX.matcher(email);
+        return matcher.find();
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
