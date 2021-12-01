7a5903b8fce110830c024d55ce2392d14002aa56
cos345 <craig.osullivan@ymail.com>
2021-12-01 09:33:30 +0000

    Built initial GUI with e-mail and password input fields

--- src/main/java/com/example/assignment3/RegistrationForm.java
+++ src/main/java/com/example/assignment3/RegistrationForm.java
@@ -17,28 +17,25 @@ import javafx.stage.Window;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    private Stage primaryStage;
+public class RegistrationForm extends Application {
+    private Stage stage;
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("JavaFX Welcome");
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Registration Form");
 
-        primaryStage.show();
+        stage.show();
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-
         Label email = new Label("Email:");
         grid.add(email, 0, 1);
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
+        TextField emailTextField = new TextField();
+        grid.add(emailTextField, 1, 1);
 
         Label pw = new Label("Password:");
         grid.add(pw, 0, 2);
@@ -46,23 +43,20 @@ public class HelloApplication extends Application {
         PasswordField pwBox = new PasswordField();
         grid.add(pwBox, 1, 2);
 
-        Button btn = new Button("Sign in");
+        Button btn = new Button("Register");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
 
-
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                if (email.getText().isEmpty()) {
-                    showAlert(Alert.AlertType.ERROR, grid.getScene().getWindow(),
-                            "Form Error!", "Please enter your email id");
-                    return;
-                }
+                showAlert(Alert.AlertType.CONFIRMATION, grid.getScene().getWindow(), "Registration Successful!", "Welcome " + emailTextField.getText());
             }
         });
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
     }
 
     private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
